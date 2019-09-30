34ee202e5e1bc014b745f1ed8435fa5af2de040e
Maurice <MauriceNgassa.Happi@antrago.de>
2019-09-30 10:25:13 +0200

    fd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable, interval } from 'rxjs';
+import { map, takeWhile, finalize } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'AsyncDataBinding';
+export class AppComponent implements OnInit {
+  countCompleted = false;
+  countDown = 5;
+  count$: Observable<number>;
+  message = 'Happy Birthday!';
+
+  ngOnInit() {
+    const timer = interval(1000);
+    this.count$ = timer.pipe(
+      map(i => this.countDown - i),
+      takeWhile(i => i > 0),
+      finalize(() => (this.countCompleted = true))
+    );
+  }
 }
